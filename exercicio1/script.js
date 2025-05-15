584108b9b62a999e5b7f4716d49a957f481f89a6
alexpampuchfaria <pampuchalexfernando@gmail.com>
2025-05-15 12:22:45 -0300

    Add files via upload

---
+++ exercicio1/script.js
@@ -0,0 +1,25 @@
+let num1;
+let num2;
+let resultado;
+let parouimpar
+
+num1 = Number(window.prompt("digite um numero"));
+num2 = Number(window.prompt("digite outro numero"));
+ 
+resultado = num1 + num2;
+
+parouimpar = resultado % 2;
+
+if(parouimpar == 0){
+    alert("o número " + resultado +" é par")
+}else {
+    alert("o número " + resultado + " é impar")
+}
+
+alert(resultado)
+
+//DESAFIO:informe na frase se o número é par ou impar.
+
+
+
+
