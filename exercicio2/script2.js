a04bbebb00896d7abe5e2566ef5c24b1bdc3e637
alexpampuchfaria <pampuchalexfernando@gmail.com>
2025-05-22 09:41:41 -0300

    Add files via upload

---
+++ exercicio2/script2.js
@@ -0,0 +1,39 @@
+function somar() {
+  let num1 = Number(window.prompt("digite um numero"))
+  let num2 = Number(window.prompt("digite outro numero"));
+
+  let resultado = num1 + num2;
+
+  alert(resultado);
+}
+function subtrair(){
+  let num1 = Number(window.prompt("digite um numero"))
+  let num2 = Number(window.prompt("digite outro numero"));
+
+  let resultado = num1 - num2;
+
+  alert(resultado);
+
+}
+
+function dividir(){
+
+  let num1 = Number(window.prompt("digite um numero"))
+  let num2 = Number(window.prompt("digite outro numero"));
+
+  let resultado = num1 / num2;
+
+  alert(resultado);
+
+
+}
+function multiplicacao(){
+
+  let num1 = Number(window.prompt("digite um numero"))
+  let num2 = Number(window.prompt("digite outro numero"));
+
+  let resultado = num1 * num2;
+
+  alert(resultado);
+
+}
