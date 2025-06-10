4c95f5eac0aa36c7699567fb2e477a412aaf8e33
alexpampuchfaria <pampuchalexfernando@gmail.com>
2025-06-10 08:50:54 -0300

    Add files via upload

---
+++ exercicio5/script5.js
@@ -0,0 +1,21 @@
+function tentar(){
+let num1;               
+let num2;
+let resultado;
+let maiormenor;
+
+num1 = Number(window.prompt("digite um numero"));
+num2 = Number(window.prompt("digite outro numero"));
+ 
+resultado = num1 > num2;
+
+
+if(maiormenor = num1 > num2){
+    alert("o número " + maiormenor +" maior")
+}else {
+    alert("o número " + maiormenor + " menor")
+}
+
+alert(resultado)
+
+}
