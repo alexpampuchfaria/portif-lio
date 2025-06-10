2764550e80943963e016e56f876ef3de1590e442
alexpampuchfaria <pampuchalexfernando@gmail.com>
2025-06-10 12:27:56 -0300

    Add files via upload

--- exercicio4/script4.js
+++ exercicio4/script4.js
@@ -1,10 +1,14 @@
 let inscritos = 0;
+let equipes = 0;
 
 contador = 0;
 function calcular() {
-  contador += 1;
+  inscritos+= 1;
+  equipes =  parseint(inscritos / 3)
+    alert( " possuem " + inscritos+ " restam" + equipes);
 
-    alert( " possuem " + contador + " restam");
+    if(equipes >= 4){
+      alert("é possivel formar 4 equipes. o campeonato pode iniciar1");
+    }
 
-    
 }
---
+++ exercicio6/script6.js
@@ -0,0 +1,21 @@
+let cliques = 0; //criar variavel//
+let mostraHTML = document.getElementById("mostraHTML");
+
+//nao precisa do msmo nome q a div do html//
+
+function contador(){
+  
+   cliques++;
+
+    //tem varios jeitos de fazer o msm resultado  exemplo:
+    // cliques += 1;
+    // cliques = cliques + 1;//
+
+
+
+
+    //alterar o valor do html pra motrar a variavel cliques//
+    mostraHTML.innerHTML = `<p> numeros de cliques: ${cliques} </p>`;
+    console.log(cliques);
+    
+}
