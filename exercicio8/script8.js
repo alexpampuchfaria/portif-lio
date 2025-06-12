2f6966452db859e63ce8dde025e3aacaa4368260
alexpampuchfaria <pampuchalexfernando@gmail.com>
2025-06-12 10:51:08 -0300

    Add files via upload

---
+++ exercicio8/script8.js
@@ -0,0 +1,23 @@
+let numsecreto;
+
+let resutado;
+let menormaior;
+
+function contador(){
+
+     numsecreto = parseInt(Math.random() * 101);
+
+     mostraHTML.innerHTML += `<p> pensei no número: ${cliques} </p>`;
+
+}
+
+  menormaior = Number(window.prompt("digite um numero"));
+  
+  numsecreto = resultado;
+
+if( numsecreto = resultado){
+     
+}else{
+
+}
+
