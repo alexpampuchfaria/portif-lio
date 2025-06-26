121adea49f164c2229421c4a41a42498f29d4011
alexpampuchfaria <pampuchalexfernando@gmail.com>
2025-06-26 10:54:19 -0300

    Add files via upload

---
+++ exercicio9/script9.js
@@ -0,0 +1,30 @@
+let mostrarHTML = document.getElementById("mostraHTML")
+let verao =("dezembro, janeiro, fevereiro");
+let outono=("março, abril, maio");
+let inverno=("junho, julho, agosto");
+let primavera =("setembro, outubro, novembro");
+let mes = prompt("digite um texto"). toUpperCase();
+
+function test(){
+if{
+        mes = ("dezembro, janeiro, fevereiro")
+        alert("a estação é " + verao)
+
+}else{
+        mes = ("março, abril, maio")
+        alert("a estação é " + outono)
+}
+}
+
+function teste(){
+        if{
+        mes = ("junho, julho, agosto")
+        alert("a estação é " + inverno)
+
+}else{
+        mes = ("setembro, outubro, novembro")
+        alert("a estação é " + outono)
+}
+
+
+}
